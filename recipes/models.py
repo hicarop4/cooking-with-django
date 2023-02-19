9b60b1f484bc1d49720019c68106e9070b3194f6
hicarop4 <pirr4lh0wface@gmail.com>
2023-02-19 17:53:55 -0300

    alter field author on recipe

--- recipes/models.py
+++ recipes/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
+    def __str__(self):
+        return f"{self.name.capitalize()}"
+    
 
 class Author(models.Model):
     author = models.CharField(max_length=65)
@@ -22,7 +26,7 @@ class Recipe(models.Model):
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
 
 
