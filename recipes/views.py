3a651a2e724a7de865b243b8708b79a8369ac1c0
hicarop4 <pirr4lh0wface@gmail.com>
2023-02-19 11:37:57 -0300

    base template + dynamic values in template

--- recipes/views.py
+++ recipes/views.py
@@ -3,9 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils import factory
 
 # HTTP REQUEST
 def home(req):
     return render(req, 'recipes/pages/home.html', context={
-        'name': 'Hícaro Vitor'
+        'recipes': [factory.make_recipe() for _ in range(10)]
+    })
+
+def recipe(req, id):
+    return render(req, 'recipes/pages/recipe-view.html', context={
+        'recipe': factory.make_recipe(),
+        'detail_page': True
     })
