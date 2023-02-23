4149257d36c2a612c2e4a58b1894dbc5de1638bc
hicarop4 <pirr4lh0wface@gmail.com>
2023-02-23 10:48:44 -0300

    change queryset to get_list_or_404 function

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,8 @@
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils import factory
 from .models import Recipe
+from django.http import HttpResponse
 
 # HTTP REQUEST
 def home(req):
@@ -11,14 +12,22 @@ def home(req):
     })
 
 def category(req, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id, is_published=True
+        )
+        .order_by('-id')
+    )
+
     return render(req, 'recipes/pages/category.html', context={
-        'recipes': recipes
+        'recipes': recipes, # get all recipes with this category
+        'title': recipes[0].category.name # get category title
     })
 
 
 def recipe(req, id):
-    recipe = Recipe.objects.get(id=id)
+    recipe = get_object_or_404(Recipe.objects, id=id)
+
     return render(req, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'detail_page': True
